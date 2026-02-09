ec3fb457f72825f0f1b67f18a80f0a71a0fea98e
Documenter.jl <documenter@juliadocs.github.io>
2026-02-09 16:01:49 +0000

    build based on d1275f3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"#Geodynamics.jl","page":"Home","title":"Geodynamics.jl","text":"Models of volcanic deformation, crustal mechanics, and tectonic processes implemented in ModelingToolkit.jl.","category":"section"}]
+}
